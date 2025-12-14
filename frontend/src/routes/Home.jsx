285c10c54bb9bc2ce41e0ae0682f22efce970fee
Chaitanya Raj <rajchaitanya75@gmail.com>
2025-12-14 16:06:01 +0530

    Dashboard completed

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -6,12 +6,15 @@ import axios from "axios";
 import server_url from "./componenets/server_url";
 import Redirect from "./componenets/Redirect";
 import AdminDashboard from "./componenets/AdminDashboard";
+import StudentDashboard from "./componenets/StudentDashboard";
 
 
 function Dashboard({ user, setUser }) {
     
 
     if (!user) return <Redirect />;
+
+    if (user.user_type === 'student') return <StudentDashboard user={user} setUser={setUser} />;
 
     return <AdminDashboard user={user} setUser={setUser} />;
 }
