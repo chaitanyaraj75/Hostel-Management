7f0da239c1fb9bf57d8a8ae611f5ea98da6da119
Chaitanya Raj <rajchaitanya75@gmail.com>
2025-12-15 02:00:54 +0530

    checks

--- frontend/src/routes/componenets/server_url.js
+++ frontend/src/routes/componenets/server_url.js
@@ -1,4 +1,4 @@
 const server_url = import.meta.env.VITE_SERVER_URL || 'https://hostel-management-backend-r960.onrender.com';
-
+console.log(server_url);
 export default server_url;
 // http://localhost:5000/api/hostel_rooms/room_requests
